345e5cccfd9a6c8b126773e68a4e3dfbd3b6d2ca
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-08-06 21:16:56 +0000

    Update copy_my_photo.sh

--- copy_my_photo.sh
+++ copy_my_photo.sh
@@ -23,11 +23,20 @@ cd "$source_path" || exit 1
 find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) | while read -r file_path; do
     # 使用basename命令获取文件名（不含路径）
     file_name=$(basename "$file_path")
-    # 使用UUID生成一个随机字符串，并拼接到文件名前面
-    # 这样可以确保重名文件不会冲突
-    new_file_name="$(uuidgen)_$file_name"
+    destination_file_path="$destination_path/$file_name"
+
+    # 检查文件是否存在，如果存在，加上_2、_3等以防止名称冲突
+    counter=1
+    while [ -e "$destination_file_path" ]; do
+        base_name=$(basename "$file_name" | cut -d. -f1)
+        extension=$(basename "$file_name" | cut -d. -f2)
+        counter=$((counter+1))
+        file_name="${base_name}_$counter.$extension"
+        destination_file_path="$destination_path/$file_name"
+    done
+
     # 使用cp命令将文件拷贝到目标路径
-    cp "$file_path" "$destination_path/$new_file_name"
+    cp "$file_path" "$destination_file_path"
 done
 
 echo "拷贝完成！"
@@ -43,7 +52,7 @@ find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec s
 | while read -r duplicate_file; do
     # 删除重复文件
     echo "删除重复文件: $duplicate_file"
-    rm "$destination_path/$duplicate_file"
+    rm "$duplicate_file"
 done
 
 echo "重复图片已去除。"
