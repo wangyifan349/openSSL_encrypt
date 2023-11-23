f479086252f18f07a1d9d0bc35f2b2e9adf8d452
wangyifan349 <86041125+wangyifan349@users.noreply.github.com>
2023-11-23 11:18:20 +0000

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-# 询问加密文件夹位置
-read -p "输入加密文件夹的路径： " encrypted_folder
-
-# 检查并授予加密文件夹的读取权限
-if [[ ! -r "$encrypted_folder" ]]; then
-    echo "Adding read permission to the encrypted folder..."
-    chmod +r "$encrypted_folder"
+# 询问解密路径
+read -p "输入被解密的路径: " encrypted_folder
+# 检查加密文件夹是否存在
+if [[ ! -d "$encrypted_folder" ]]; then
+    echo "指定的文件夹不存在，请检查路径。"
+    exit 1
 fi
 
-# 要求输入解密密码
-read -sp "Enter the decryption password: " decryption_password
+# 请求输入解密密码
+read -sp "输入解密密码： " decryption_password
 echo
 
-# 创建一个临时文件夹来存储解密的文件
+# 创建临时文件夹用于存储解密文件
 temp_folder=$(mktemp -d)
 
-# 使用 AES-256-CBC 和 PBKDF2 解密文件并覆盖加密文件
-cd "$encrypted_folder"
-for file in *; do
-    if [[ ! -w "$file" ]]; then
-        echo "Adding write permission to '$file'..."
-        chmod +w "$file"
-    fi
+# 进入加密文件夹
+cd "$encrypted_folder" || exit 1
 
-    openssl enc -d -aes-256-cbc -pbkdf2 -in "$file" -out "$temp_folder/$file" -k "$decryption_password"
-    mv "$temp_folder/$file" "$file"
+# 使用AES-256-CBC算法解密所有加密文件，并将解密后的文件存放在临时文件夹中
+find . -type f -name "*.aes" -print0 | while IFS= read -r -d '' file; do
+    original_file="${file%.aes}" # 移除.aes扩展名以得到原始文件名
+    # 解密每个文件并将其存储在临时文件夹中
+    openssl enc -aes-256-cbc -pbkdf2 -d -salt -in "$file" -out "$temp_folder/$original_file" -k "$decryption_password" && {
+        # 如果解密成功，则用解密文件替换加密文件
+        mv -f "$temp_folder/$original_file" "$original_file"
+        echo "已解密: $original_file"
+    } || echo "解密失败: $file"
 done
 
 # 删除临时文件夹
 rm -r "$temp_folder"
 
-echo "解密完成。 所有文件位于 '$encrypted_folder' 已被解密并覆盖"
+echo "解密完成。所有文件在 '$encrypted_folder' 已经被解密并覆盖。"
